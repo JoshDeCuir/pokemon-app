38eb415321e11b17534d7622720f17ca4c1249e1
JoshDeCuir <joshmdecuir@gmail.com>
2023-09-20 22:17:46 -0500

    Updated CSS, JS, and Index
    Cleaned up the code and added comments where it was needed.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,21 @@
-var pokemonList = [{name:"Bulbasaur", height:'7', type: ['grass, poison']},
-                {name:"Charmander", height: '6', type: 'fire'},
-                {name:"Squirtle", height:'5', type: 'water'}
-            ];
+var pokemonList = [
+    {name:"Bulbasaur", height: 0.7, type: ['Grass , Poison']},
+    {name:"Charmander", height: 0.6, type: 'Fire'},
+    {name:"Charizard", height: 1.7, type: ['Flying , Fire']},
+    {name:"Squirtle", height: 0.5, type: 'Water'}
+];
+//Define the height threshold 
+var bigPokemon = 1.5;
+
+for(var i = 0; i < pokemonList.length; i++){
+    var pokemon = pokemonList[i];
+    var output = pokemon.name + " (height: " + pokemon.height + ")";
+    
+    if(pokemon.height > bigPokemon){
+        output += 'Wow, thats big!';
+    }
+     // Display the Pokémon information with the special label
+    document.write("<p>" + output + "</p>");
+    
+    document.getElementById("output").innerHTML += `Name: ${pokemon.name}, Height: ${pokemon.height}, Type: ${pokemon.type}<br>`;
+}
