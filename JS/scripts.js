5fbd13f024f932cbafa87b3091b3ac97eaba708c
JoshDeCuir <joshmdecuir@gmail.com>
2023-09-27 21:09:20 -0500

    Update scripts.js
    Created pokemonRepository

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,22 @@
-var pokemonList = [
-    {name:"Bulbasaur", height: 0.7, type: ['Grass , Poison']},
-    {name:"Charmander", height: 0.6, type: 'Fire'},
-    {name:"Charizard", height: 1.7, type: ['Flying , Fire']},
-    {name:"Squirtle", height: 0.5, type: 'Water'}
-];
+var pokemonRepository = (function (){
+    var pokemonList = [
+        {name:"Bulbasaur", height: 0.7, type: ['Grass , Poison']},
+        {name:"Charmander", height: 0.6, type: 'Fire'},
+        {name:"Charizard", height: 1.7, type: ['Flying , Fire']},
+        {name:"Squirtle", height: 0.5, type: 'Water'}
+    ];
+    return{
+        add: function(pokemon){
+            pokemonList.push(pokemon);
+        },
+        getAll: function(){
+            return pokemonList;
+        }
+    };
+})();
+
 //Define the height threshold 
-var bigPokemon = 1.5;
+//var bigPokemon = 1.5;
 
 //for(var i = 0; i < pokemonList.length; i++){
     //var pokemon = pokemonList[i];
@@ -19,7 +30,8 @@ var bigPokemon = 1.5;
     
     //document.getElementById("output").innerHTML += `Name: ${pokemon.name}, Height: ${pokemon.height}, Type: ${pokemon.type}<br>`;
 //}
-pokemonList.forEach(function(pokemon){
-    document.getElementById("output").innerHTML += 'Name:' + pokemon.name + ' , Height:' + pokemon.height + ' , Type:' + pokemon.type + '<br>'
-    
+pokemonRepository.add({name: "Evee", height: 0.2, type: 'Normal'});
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    document.getElementById("output").innerHTML += 'Name:' + pokemon.name + ' , Height:' + pokemon.height + ' , Type:' + pokemon.type + '<br>';
 });
