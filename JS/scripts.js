9b561664d70ac2af762e1c317878d90e3ce6d1c2
JoshDeCuir <joshmdecuir@gmail.com>
2023-10-08 19:49:25 -0500

    Update scripts.js
    Added API list

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,6 @@
 var pokemonRepository = (function (){
-    var pokemonList = [
-        {name:'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
-        {name:'Charmander', height: 0.6, type: ['Fire']},
-        {name:'Charizard', height: 1.7, type: ['Flying', 'Fire']},
-        {name:'Squirtle', height: 0.5, type: ['Water']}
-    ];
+    var pokemonList = [];
+    var pokemonAPI ="https://pokeapi.co/api/v2/pokemon/?limit=150";
     
     function addListItem(pokemon){
         var pokemonList = document.querySelector(".pokemon-list");
@@ -17,26 +13,71 @@ var pokemonRepository = (function (){
         button.classList.add("button-class");
         listPokemon.appendChild(button);
         pokemonList.appendChild(listPokemon);
-    }
-    function showDetails(pokemon){
-        console.log(pokemon);
+        button.addEventListener("click", function(event){
+            showDetails(pokemon);
+        });
     }
     function add(pokemon){
-        pokemonList.push(pokemon);
+        if(
+            typeof pokemon === "object" &&
+            'name' in pokemon
+        ){
+            pokemonList.push(pokemon);
+        } else{
+            console.log('Pokemon is not correct.')
+        }
     }
+    function loadList() {
+        return fetch(pokemonAPI).then(function (response){
+            return response.json();
+        }).then(function(json){
+            json.results.forEach(function(item){
+                var pokemon ={
+                    name: item.name,
+                    detailsURL: item.url
+                };
+                add (pokemon);
+            });
+        }).catch(function(e){
+            console.error(e);
+        });
+    }
+
+    function loadDetails (item){
+        var url = item.detailsURL;
+        return fetch(url).then(function(response){
+            return response.json();
+        }).then(function (details) {
+            item.imageUrl = details.sprites.font_dafult;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
+    function showDetails(item){
+    loadDetails(item).then(function() 
+        {
+            console.log(item);
+        });
+    }
+
     function getAll(){
         return pokemonList;
     }
     return{
         add: add,
         getAll: getAll,
-        addListItem: addListItem
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails
     }
 })();
 
-pokemonRepository.add({name: 'Evee', height: 0.2, type: ['Normal']});
-
-pokemonRepository.getAll().forEach(function(pokemon){
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+    });
 });
-
